a6d42d779d98cf1c1d7ab686e13363313362efa6
Brian C <brianlc@sowired.info>
2026-09-10 21:19:41 -0700

    Initial Commit

---
+++ ImageSherpa/ImageSherpaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSherpaApp.swift
+//  ImageSherpa
+//
+//  Created by Brian Cosgrove on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageSherpaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
